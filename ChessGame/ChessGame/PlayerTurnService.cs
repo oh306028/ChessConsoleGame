1b4de87eaa7b8ba1a4349207724eeba39f9295a6
Oskar <hamerlao@gmail.com>
2024-02-16 00:16:40 +0100

    PlayerTurnServiceAdded

---
+++ ChessGame/ChessGame/PlayerTurnService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessGame
+{
+    public class PlayerTurnService
+    {
+        public bool WhitePlayerCanMove(IPawn pawn)  
+        {
+            if(char.IsUpper(pawn.symbol))
+            return true;    
+
+            return false;
+        }
+
+    }
+}
